c290b5095527b6c33e207ec3432e0bececf190e3
mcherrier <98856418+mcherrier@users.noreply.github.com>
2022-04-27 23:20:19 -0800

    added main loop

--- main.py
+++ main.py
@@ -6,28 +6,45 @@
 #################################################
 
 #Hard coded value and weight arrays
-import util, random
+import util, random, numpy
+NUM_GENERATIONS = 100
+best_generation = 0
+
 vals = [300,1500,100,1000,3425,50,850,550,15,2000]
 weights = [15,20,8,25,12,15,12,9,18,14]
 population_size = 10
 num_objects = 10
+
 #Initiate population of critters
 critters = util.initPop(population_size, num_objects)
 childcritters = util.mutate(critters)
 
+#main loop
+best_gen_value = 0
+for i in range(NUM_GENERATIONS):
+    #get children, mutate and append
+    child_critters = util.mutate(critters)
+    critters = critters + child_critters
 
-for i in range(len(critters)):
-    print("Critter: ", critters[i])
-    print("Child:   ", childcritters[i])
+    #get best children and their values
+    critters = util.getBest(critters, vals, weights)
+    values = util.getVals(critters, vals, weights)
+
+    #get best value from the set, and use it to find the best critter in the set
+    best_val = numpy.amax(values)
+    bv_index = values.index(best_val)
+    best_critter = critters[bv_index]
+
+    if best_val > best_gen_value:
+        best_generation = i+1
+
+    print("Generation: ", i+1)
+    print("Best critter: ", best_critter)
+    print("Value:   ", best_val)
     print("")
 
-critters += childcritters
-values = util.getVals(critters, vals, weights)
-for i in range(len(critters)):
-    print("Critter: ", critters[i])
-    print("Value:   ", values[i])
-    print("")
+
+print("Best Overall Generation: ", best_generation)
 
 #TODO APPLY MUTATION OPERATION
-
 
